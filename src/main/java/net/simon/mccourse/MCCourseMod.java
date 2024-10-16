e495ea74c0f89c9c7b9d205697f1c67965bbe3b0
simo12321 <scholzsimon6@gmail.com>
2024-10-16 10:28:17 +0200

    some tests

--- src/main/java/net/simon/mccourse/MCCourseMod.java
+++ src/main/java/net/simon/mccourse/MCCourseMod.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
+
 @Mod(MCCourseMod.MOD_ID)
 public class MCCourseMod {
     // Define mod id in a common place for everything to reference
